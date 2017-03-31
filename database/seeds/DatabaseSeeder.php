7d72e1516b9a0f35103ee14d6181a10a74432ccd
sc0Vu <alk03073135@gmail.com>
2017-03-31 16:31:18 +0800

    change database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call('ArticlesTableSeeder');
         $this->call('RolesTableSeeder');
         $this->call('RoleAssignsTableSeeder');
+        $this->call('CategoriesTableSeeder');
+        $this->call('CategoryRelationshipsTableSeeder');
     }
 }
